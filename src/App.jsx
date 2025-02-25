02c0e33c2ca6442d91d564db891b5d94cf37bcde
iamAlinaaa <alinaaevns1806@gmail.com>
2025-02-25 16:55:34 +0200

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import UserManagement from "./pages/admin/UserManagement/UserManagement";
 import LoginPage from "./pages/login/LoginPage";
 import ForgotPasswordPage from "./pages/passwordRecovery/ForgotPasswordPage";
 import ResetPassword from "./pages/passwordRecovery/ResetPassword";
+import HomePage from "./pages/home/HomePage";
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/user/portal" element={<UserPortal />} />
           <Route path="/admin/portal" element={<AdminPortal />} />
           <Route path="/admin/user-management" element={<UserManagement />} />
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import burgerMenuIcon from "../../assets/homepage-icons/burger-menu.svg";
+import keyGraphicIcon from "../../assets/homepage-icons/KeyGraphic.svg";
+import mainDoorGraphicIcon from "../../assets/homepage-icons/MainDoorGraphic.svg";
+import redkeyLogoIcon from "../../assets/homepage-icons/Redkey_Logo.svg";
+import teamAntrimIcon from "../../assets/homepage-icons/Team_Antrim.svg";
+import teamLafitteIcon from "../../assets/homepage-icons/Team_Lafitte.svg";
+import teamPrytaniaIcon from "../../assets/homepage-icons/Team_Prytania.svg";
+import teamPacificIcon from "../../assets/homepage-icons/Teams_Pacific.svg";
+
+import "./styles.scss";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <div className="home-page-header">
+        <img src={burgerMenuIcon} className="burger-menu-icon" />
+        <img src={redkeyLogoIcon} className="redkey-logo-icon" />
+      </div>
+      <div className="home-page-container">
+        <img src={mainDoorGraphicIcon} className="main-door-home-icon" />
+      </div>
+
+      <div className="home-login-button-wrapper">
+        <img src={keyGraphicIcon} className="key-graphic-icon" />
+        <p className="request-access-login-button">Request access</p>
+        <hr className="horizontal-line-login-button" />
+        <p className="request-access-login-button" onClick={handleLoginClick}>
+          Login
+        </p>
+      </div>
+
+      <div className="teams-info-wrapper">
+        <div>
+          <p className="teams-info-main-title">The redkey research teams</p>
+          <div className="teams-items-wrapper">
+            <div className="team-item-container">
+              <img src={teamPacificIcon} className="team-icon" />
+              <p className="team-item-title">Pacific Square Research</p>
+              <p className="team-item-description">
+                Experts in forensic and fundamental analysis, uncovering
+                companies with overstretretched business models and financial
+                discrepancies.
+              </p>
+            </div>
+            <div className="team-item-container">
+              <img src={teamLafitteIcon} className="team-icon" />
+              <p className="team-item-title">Lafitte Research</p>
+              <p className="team-item-description">
+                Focused on software sector short research, providing
+                high-conviction ideas through proprietary data gathering.
+              </p>
+            </div>
+            <div className="team-item-container">
+              <img src={teamPrytaniaIcon} className="team-icon" />
+              <p className="team-item-title">Prytania Research</p>
+              <p className="team-item-description">
+                Generating actionable short ideas in non-therapeutic healthcare
+                through deep industry diligence.
+              </p>
+            </div>
+            <div className="team-item-container">
+              <img src={teamAntrimIcon} className="team-icon" />
+              <p className="team-item-title">Antrim Research</p>
+              <p className="team-item-description">
+                Specializing in sourcing short ideas across cyclicals, offering
+                insight into emerging market shifts.
+              </p>
+            </div>
+          </div>
+          <footer className="footer-home-page">© 2025 Redkey Research</footer>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
